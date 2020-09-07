aed77d2bce13c69585527164d6bda034c6e207c4
Flux1960 <70839036+Flux1960@users.noreply.github.com>
2020-09-07 12:28:53 +0800

    创建了新项目

---
+++ 10-30-Playing-Cards/10-30-Playing-CardsView.cpp
@@ -0,0 +1,127 @@
+
+// 10-30-Playing-CardsView.cpp : CMy1030PlayingCardsView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "10-30-Playing-Cards.h"
+#endif
+
+#include "10-30-Playing-CardsDoc.h"
+#include "10-30-Playing-CardsView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMy1030PlayingCardsView
+
+IMPLEMENT_DYNCREATE(CMy1030PlayingCardsView, CView)
+
+BEGIN_MESSAGE_MAP(CMy1030PlayingCardsView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CMy1030PlayingCardsView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CMy1030PlayingCardsView 构造/析构
+
+CMy1030PlayingCardsView::CMy1030PlayingCardsView()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMy1030PlayingCardsView::~CMy1030PlayingCardsView()
+{
+}
+
+BOOL CMy1030PlayingCardsView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMy1030PlayingCardsView 绘制
+
+void CMy1030PlayingCardsView::OnDraw(CDC* /*pDC*/)
+{
+	CMy1030PlayingCardsDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMy1030PlayingCardsView 打印
+
+
+void CMy1030PlayingCardsView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CMy1030PlayingCardsView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CMy1030PlayingCardsView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CMy1030PlayingCardsView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CMy1030PlayingCardsView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMy1030PlayingCardsView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMy1030PlayingCardsView 诊断
+
+#ifdef _DEBUG
+void CMy1030PlayingCardsView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMy1030PlayingCardsView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMy1030PlayingCardsDoc* CMy1030PlayingCardsView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMy1030PlayingCardsDoc)));
+	return (CMy1030PlayingCardsDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMy1030PlayingCardsView 消息处理程序
---
+++ 10-30-Playing-Cards/10-30-Playing-CardsView.h
@@ -0,0 +1,52 @@
+
+// 10-30-Playing-CardsView.h : CMy1030PlayingCardsView 类的接口
+//
+
+#pragma once
+
+
+class CMy1030PlayingCardsView : public CView
+{
+protected: // 仅从序列化创建
+	CMy1030PlayingCardsView();
+	DECLARE_DYNCREATE(CMy1030PlayingCardsView)
+
+// 特性
+public:
+	CMy1030PlayingCardsDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMy1030PlayingCardsView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // 10-30-Playing-CardsView.cpp 中的调试版本
+inline CMy1030PlayingCardsDoc* CMy1030PlayingCardsView::GetDocument() const
+   { return reinterpret_cast<CMy1030PlayingCardsDoc*>(m_pDocument); }
+#endif
+
